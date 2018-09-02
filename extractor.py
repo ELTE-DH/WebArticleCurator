ed17ab6806d704897df91595c802ca732ae454db
Balázs Indig <indba@digitus.itk.ppke.hu>
2018-09-02 18:43:40 +0200

    Convert main() to class: initial layout

--- extractor.py
+++ extractor.py
@@ -13,11 +13,11 @@ def extract_article_urls_from_page(article_list_raw_html, settings):
     """
         extracts and returns as a list the URLs belonging to articles from an HTML code
     """
-    urls = []
+    urls = set()
     for code_line in settings['ARTICLE_LINK_FORMAT_RE'].findall(article_list_raw_html):
         code_line = settings['BEFORE_ARTICLE_URL_RE'].sub('', code_line)
         code_line = settings['AFTER_ARTICLE_URL_RE'].sub('', code_line)
-        urls.append(code_line)
+        urls.add(code_line)
     return urls
 
 
--- news_crawler.py
+++ news_crawler.py
@@ -13,98 +13,108 @@ from enhanced_downloader import WarcDownloader, WarcReader
 from extractor import extract_article_urls_from_page, articles_to_corpus
 
 
-def main():
-    """
-    get the command line argument
-    it is the configuration file's name
-    """
-    try:
-        current_task_config_filename = sys.argv[1]
-    except IndexError:
-        raise IndexError('Not enough or too many input arguments!\n'
-                         'The program should be called like:\n'
-                         'python web_crawler.py current_task_config.json')
+class NewsCrawler:
+    def __init__(self):
+        """
+        get the command line argument
+        it is the configuration file's name
+        """
+        try:
+            current_task_config_filename = sys.argv[1]
+        except IndexError:
+            raise IndexError('Not enough or too many input arguments!\n'
+                             'The program should be called like:\n'
+                             'python web_crawler.py current_task_config.json')
 
-    # read input data from the given files, initialize variables
-    settings = wrap_input_consants(current_task_config_filename)
-    file_out = open(settings['output_file'], 'a+', encoding=settings['encoding'])
-    logger_ = Logger(settings['log_file'])
-    converter = CorpusConverter(settings['site_schemas'], settings['tags'])
-    filename = 'example.warc.gz'
-    download == True
-    if download == True:
-        downloader = WarcDownloader('example.warc.gz', logger_)
-    else:
-        downloader = WarcReader('example.warc.gz', logger_)
+        # read input data from the given files, initialize variables
+        self._settings = wrap_input_consants(current_task_config_filename)
+        self._file_out = open(self._settings['output_file'], 'a+', encoding=self._settings['encoding'])
+        self._logger_ = Logger(self._settings['log_file'])
+        self._converter = CorpusConverter(self._settings['site_schemas'], self._settings['tags'])
+        self._max_pagenum = -1
+        self._good_urls = set()
+        self._problematic_urls = set()
+        self._urls_to_download = set()
+        filename = 'example.warc.gz'
+        download == True
+        if download == True:
+            self._downloader = WarcDownloader('example.warc.gz', logger_)
+        else:
+            self._downloader = WarcReader('example.warc.gz', logger_)
 
-    # generate URLs of lists of articles
-    # extract URLs of articles from these lists
-    # download article pages
-    # convert these articles from raw HTML to corpus format
+    def __del__(self):
+        self._file_out.close()
 
-    # if URLs of current website's archive pages are generated based on an ID,
-    # which is a positive integer growing one by one
-    article_list_urls = []
-    max_pagenum = -1
-    if settings['ARTICLE_LIST_URLS_BY_DATE']:  # e.g. index.hu, origo.hu
-        max_pagenum = 0  # No pages (may be overwritten e.g. origo.hu)
-        article_list_urls = [gen_article_list_url_from_date(settings['DATE_FROM'], timedelta(days=curr_day), settings)
-                             for curr_day in range(settings['INTERVAL'].days + 1)]
+    def url_iterator(self):
 
-    if settings['ARTICLE_LIST_URLS_BY_ID']:
-        max_pagenum = -1  # Infinty
-        # not URL_BY_DATE and URLS_BY_ID
-        if len(article_list_urls) == 0:  # e.g. 444.hu
-            article_list_urls.append(settings['article_list_url_format'])
+        # generate URLs of lists of articles
+        # extract URLs of articles from these lists
+        # download article pages
+        # convert these articles from raw HTML to corpus format
 
-    if len(article_list_urls) == 0:
-        print('There is no existing case where settings[\'ARTICLE_LIST_URLS_BY_DATE\'] and'
-              ' settings[\'ARTICLE_LIST_URLS_BY_ID\'] are both False')  # try/excepttel csinálni?
-        return
+        # if URLs of current website's archive pages are generated based on an ID,
+        # which is a positive integer growing one by one
+        article_list_urls = []
 
-    for article_list_url in article_list_urls:
-        gen_corpus_from_id_url_list_w_subpages(article_list_url, converter, file_out, downloader, logger_, settings,
-                                               max_pagenum)
+        if self._settings['ARTICLE_LIST_URLS_BY_DATE']:  # e.g. index.hu, origo.hu
+            self._max_pagenum = 0  # No pages (may be overwritten e.g. origo.hu)
+            date_from = self._settings['DATE_FROM']
+            url_format = self._settings['article_list_url_format']
+            article_list_urls = [self._gen_article_list_url_from_date(date_from, timedelta(days=curr_day), url_format)
+                                 for curr_day in range(self._settings['INTERVAL'].days + 1)]
 
-    file_out.close()
+        if self._settings['ARTICLE_LIST_URLS_BY_ID']:
+            self._max_pagenum = -1  # Infinty
+            # not URL_BY_DATE and URLS_BY_ID
+            if len(article_list_urls) == 0:  # e.g. 444.hu
+                article_list_urls.append(self._settings['article_list_url_format'])
 
+        if len(article_list_urls) == 0:
+            print('There is no existing case where settings[\'ARTICLE_LIST_URLS_BY_DATE\'] and'
+                  ' settings[\'ARTICLE_LIST_URLS_BY_ID\'] are both False')  # try/excepttel csinálni?
+            return
 
-def gen_article_list_url_from_date(date_from, delta_day, settings):
-    """
-        generates and returns the URLs of a page the contains URLs of articles published that day
-    """
-    current_year, current_month, current_day = '{0}{1}'.format(date_from,  delta_day).split('-', maxsplit=2)
-    article_list_url = settings['article_list_url_format'].replace('#year', current_year).\
-        replace('#month', current_month).replace('#day', current_day)
-    return article_list_url
+        for article_list_url in article_list_urls:
+            self._gen_corpus_from_id_url_list_w_subpages(article_list_url)  # TODO: yield from...
 
+    @staticmethod
+    def _gen_article_list_url_from_date(date_from, delta_day, url_format):
+        """
+            generates and returns the URLs of a page the contains URLs of articles published that day
+        """
+        curr_year, curr_month, curr_day = '{0}{1}'.format(date_from,  delta_day).split('-', maxsplit=2)
+        art_list_url = url_format.replace('#year', curr_year).replace('#month', curr_month).replace('#day', curr_day)
+        return art_list_url
 
-def gen_corpus_from_id_url_list_w_subpages(article_list_url, converter, file_out, downloader, logger_, settings,
-                                           max_pagenum):
-    """
-        generates corpus from a URL regarding to a sub-pages that contains article URLs
-    """
-    # Download the first page
-    article_list_raw_html, article_list_only_urls = \
-        gen_corpus_from_id_url_list(article_list_url, converter, file_out, downloader, logger_, settings)
-    page_num = 0
-    # if the last article list page does not contain any links to articles, go to next date
-    while article_list_raw_html is not None and len(article_list_only_urls) > 0 and page_num < max_pagenum:
-        page_num += 1
-        article_list_url = '{0}{1}'.format(article_list_url, page_num)
-        article_list_raw_html, article_list_only_urls = \
-            gen_corpus_from_id_url_list(article_list_url, converter, file_out, downloader, logger_, settings)
+    def _gen_corpus_from_id_url_list_w_subpages(self, article_list_url_base):
+        """
+            generates corpus from a URL regarding to a sub-pages that contains article URLs
+        """
+        # Download the first page
+        is_last_page = self._gen_corpus_from_id_url_list(article_list_url_base)
+        page_num = 0
+        max_pagenum = self._max_pagenum
+        # if the last article list page does not contain any links to articles, go to next date
+        while is_last_page and page_num < max_pagenum:  # TODO: convert to for in range + break?
+            page_num += 1
+            article_list_url = '{0}{1}'.format(article_list_url_base, page_num)
+            is_last_page = self._gen_corpus_from_id_url_list(article_list_url)
 
-
-def gen_corpus_from_id_url_list(article_list_url, converter, file_out, downloader, logger_, settings):
-        article_list_only_urls = []
-        article_list_raw_html = downloader.download_url(article_list_url)
-        if article_list_raw_html is not None:
-            article_list_only_urls = extract_article_urls_from_page(article_list_raw_html, settings)
-            articles_to_corpus(article_list_only_urls, converter, file_out, settings, downloader, logger_)
-        return article_list_raw_html, article_list_only_urls
+    def _gen_corpus_from_id_url_list(self, article_list_url):
+            article_list_only_urls = []
+            article_list_raw_html = self._downloader.download_url(article_list_url)
+            if article_list_raw_html is not None:
+                self._good_urls.add(article_list_url)
+                article_list_only_urls = extract_article_urls_from_page(article_list_raw_html, self._settings)
+                self._urls_to_download |= article_list_only_urls  # TODO: This or iterator...
+                # articles_to_corpus(article_list_only_urls, self._converter, self._file_out, self._settings,
+                #                    self._downloader, self._logger_)
+             else:
+                self._problematic_urls.add(article_list_url)
+            return article_list_raw_html is not None and len(article_list_only_urls) > 0  # is last
 
 
 # run the whole thing
 if __name__ == '__main__':
-    main()
+    m = NewsCrawler()
+    m.url_iterator()
