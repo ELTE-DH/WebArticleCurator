423a97f52c7f0495ed8696d08fcc767599a8c578
zsofiar <zsofia.rakovics@gmail.com>
2021-10-15 09:33:12 +0200

    Minor modification

--- configs/extractors/site_specific_extractor_functions_news_ngvmt.py
+++ configs/extractors/site_specific_extractor_functions_news_ngvmt.py
@@ -743,7 +743,7 @@ def next_page_of_article_rangado_24hu(curr_html):
         current_page = int(bs.find('span', class_='page-numbers current').getText())
         other_pages = bs.find_all('a', class_='page-numbers')
         for i in other_pages:
-            if (i.find('span') is None) and (int(i.getText()) + 1 == current_page) and ('href' in i.attrs.keys()):
+            if (i.find('span') is None) and (i.getText() == str(current_page - 1)) and ('href' in i.attrs.keys()):
                 next_link = i.attrs['href']
                 return next_link
     return None
