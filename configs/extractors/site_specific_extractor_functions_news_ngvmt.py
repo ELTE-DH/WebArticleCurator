d1e633bfb55b31cf713210c989a27abae87b359c
zsofiar <zsofia.rakovics@gmail.com>
2021-10-22 13:07:40 +0200

    Updating the tests for rangado_24hu

--- configs/extractors/site_specific_extractor_functions_news_ngvmt.py
+++ configs/extractors/site_specific_extractor_functions_news_ngvmt.py
@@ -779,23 +779,29 @@ def next_page_of_article_test(filename, test_logger):
     assert next_page_of_article_merce(text) is None
 
     test_logger.log('INFO', 'Testing rangado_24hu')
+    # Example, when the number of the pages of the article is 3: 0 [3] >> 2 >> 1 >> None
+    text = w.download_url('https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/')
+    assert next_page_of_article_rangado_24hu(
+        text) == 'https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/2/'
     text = w.download_url('https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/2/')
-    assert next_page_of_article_rangado_24hu(text) == \
-           'https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/3/'
-    text = w.download_url('https://rangado.24.hu/magyar_foci/2019/06/08/eb-selejtezo-azerbajdzsan-magyarorszag/')
-    assert next_page_of_article_rangado_24hu(text) == \
-           'https://rangado.24.hu/magyar_foci/2019/06/08/eb-selejtezo-azerbajdzsan-magyarorszag/1/'
-    text = w.download_url('https://rangado.24.hu/nemzetkozi_foci/2019/05/29/chelsea-arsenal-europa-liga-donto-baku/')
-    assert next_page_of_article_rangado_24hu(text) == \
-           'https://rangado.24.hu/nemzetkozi_foci/2019/05/29/chelsea-arsenal-europa-liga-donto-baku/1/'
+    assert next_page_of_article_rangado_24hu(
+        text) == 'https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/1/'
     text = w.download_url('https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/1/')
-    assert next_page_of_article_rangado_24hu(text) == \
-           'https://rangado.24.hu/magyar_foci/2019/10/10/eb-selejtezo-horvat-magyar/2/'
+    assert next_page_of_article_rangado_24hu(text) is None
+    # Further examples
+    text = w.download_url('https://rangado.24.hu/magyar_foci/2019/06/08/eb-selejtezo-azerbajdzsan-magyarorszag/')
+    assert next_page_of_article_rangado_24hu(
+        text) == 'https://rangado.24.hu/magyar_foci/2019/06/08/eb-selejtezo-azerbajdzsan-magyarorszag/1/'
+    text = w.download_url('https://rangado.24.hu/nemzetkozi_foci/2019/05/29/chelsea-arsenal-europa-liga-donto-baku/')
+    assert next_page_of_article_rangado_24hu(
+        text) == 'https://rangado.24.hu/nemzetkozi_foci/2019/05/29/chelsea-arsenal-europa-liga-donto-baku/1/'
+    text = w.download_url(
+        'https://rangado.24.hu/nemzetkozi_foci/2019/05/01/bajnokok-ligaja-elodonto-barcelona-liverpool/1/')
+    assert next_page_of_article_rangado_24hu(text) is None
+    text = w.download_url(
+        'https://rangado.24.hu/nemzetkozi_foci/2020/03/10/bl-nyolcaddonto-leipzig-tottenham-valencia-atalanta-elo/')
+    assert next_page_of_article_rangado_24hu(text) is None
     text = w.download_url('https://rangado.24.hu/magyar_foci/2019/11/07/europa-liga-ftc-cszka-moszkva-elo/1/')
-    assert next_page_of_article_rangado_24hu(text) == \
-           'https://rangado.24.hu/magyar_foci/2019/11/07/europa-liga-ftc-cszka-moszkva-elo/2/'
-    text = w.download_url('https://rangado.24.hu/nemzetkozi_foci/2020/03/10/bl-nyolcaddonto-leipzig-tottenham-'
-                          'valencia-atalanta-elo/')
     assert next_page_of_article_rangado_24hu(text) is None
     text = w.download_url('https://rangado.24.hu/magyar_foci/2021/10/11/tenyleg-van-visszaut-boli-ujra-a-fradi-elso'
                           '-csapataval-edzett/')
